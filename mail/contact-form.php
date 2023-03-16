00862b2d476990272735c263bdf5010b6edbb958
Mohammad Tauffique Umar <80394078+tauffique@users.noreply.github.com>
2023-03-16 17:02:55 +0100

    Add files via upload

---
+++ mail/contact-form.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+  header('Access-Control-Allow-Headers: token, Content-Type');
+  header('Access-Control-Max-Age: 1728000');
+  header('Content-Length: 0');
+  header('Content-Type: text/plain');
+  die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+$to = "digitalm@sjmedia.digital"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
